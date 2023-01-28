49182974219d8f99db378b09cf14a4d085d45d50
JinSan-RM <yug6789@naver.com>
2023-01-28 14:21:52 +0900

    jsend

--- resources/js/test.js
+++ resources/js/test.js
@@ -1,9 +1,9 @@
-function mytest(){
+function mytest() {
     alert('외부 파일에서 실행 됨')
+
 }
 
-//window.onload
-
-onload=function(){
+// window.onload
+onload = function () {
     alert('page 로딩이 완료')
 }
